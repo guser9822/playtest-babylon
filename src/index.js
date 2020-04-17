ff00cc208928342502a4e2943bdfdf6451b64c75
guser9822 <riccardo.daniello13@gmail.com>
2020-04-18 00:18:14 +0200

    Created cubes

--- src/index.js
+++ src/index.js
@@ -35,24 +35,26 @@ var engine = new Engine(canvas, true);
 var scene = new Scene(engine);
 
 // The camera, necessary see the world
-var camera = new FreeCamera("camera", new Vector3(5, 5, -5), scene);
+var camera = new FreeCamera("camera", new Vector3(0, 5, -25), scene);
 camera.setTarget(Vector3.Zero());
 // The ambient light
 var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
 
 // The cube
-var cube = Mesh.CreateBox("myBox", 1, scene);
-
-// Create a grid material
-//var material = new GridMaterial("grid", scene);
-// Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-// var sphere = Mesh.CreateSphere("sphere1", 2, 2, scene);
-
-// // Move the sphere upward 1/2 its height
-// sphere.position.y = 2;
-
-// // Affect a material
-// sphere.material = material;
+const cubes = [];
+let x_pos = 0;
+let y_pos = 0;
+let z_pos = 0;
+let dimension = 1;
+const numberOfCubes = 7;
+for (const num of Array.from(Array(numberOfCubes).keys())) {
+    const cube = Mesh.CreateBox(`myCube-${num}`, dimension, scene);
+    cube.setAbsolutePosition(x_pos, y_pos, z_pos);
+    x_pos = x_pos + 2;
+    y_pos = y_pos + 1;
+    z_pos = z_pos + 2;
+    cubes.push(cube);
+}
 
 // The render loop
 engine.runRenderLoop(function () {
