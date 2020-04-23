cf9d254eb81e5a3b31906b85f9c72415ea979481
guser9822 <riccardo.daniello13@gmail.com>
2020-04-23 08:17:37 +0200

    Added debug layer

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import {
     Vector3
 } from "@babylonjs/core/Maths/math";
 import {
-    FreeCamera
-} from "@babylonjs/core/Cameras/freeCamera";
+    UniversalCamera
+} from "@babylonjs/core/Cameras/universalCamera";
 import {
     HemisphericLight
 } from "@babylonjs/core/Lights/hemisphericLight";
@@ -26,6 +26,10 @@ import {
 
 // Required side effects to populate the Create methods on the mesh class. Without this, the bundle would be smaller but the createXXX methods from mesh would not be accessible.
 import "@babylonjs/core/Meshes/meshBuilder";
+// Augments the scene with the debug methods
+import "@babylonjs/core/Debug/debugLayer";
+// Injects a local ES6 version of the inspector to prevent automatically relying on the none compatible version
+import "@babylonjs/inspector";
 
 // BABYLON Engine creation
 var canvas = document.getElementById('gameCanvas');
@@ -35,24 +39,17 @@ var engine = new Engine(canvas, true);
 var scene = new Scene(engine);
 
 // The camera, necessary see the world
-var camera = new FreeCamera("camera", new Vector3(5, 5, -5), scene);
+var camera = new UniversalCamera("camera", new Vector3(5, 5, -5), scene);
 camera.setTarget(Vector3.Zero());
+//Attach camera to the canvas
+camera.attachControl(canvas, true);
 // The ambient light
 var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
 
 // The cube
 var cube = Mesh.CreateBox("myBox", 1, scene);
+scene.debugLayer.show();
 
-// Create a grid material
-//var material = new GridMaterial("grid", scene);
-// Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-// var sphere = Mesh.CreateSphere("sphere1", 2, 2, scene);
-
-// // Move the sphere upward 1/2 its height
-// sphere.position.y = 2;
-
-// // Affect a material
-// sphere.material = material;
 
 // The render loop
 engine.runRenderLoop(function () {
