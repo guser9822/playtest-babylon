ce5d8b07f461f47e13b89bc8e58f570733ee2f48
guser9822 <riccardo.daniello13@gmail.com>
2020-05-16 20:27:06 +0200

    Added slybox

--- src/classes/materialsgen.js
+++ src/classes/materialsgen.js
@@ -1,11 +1,16 @@
 import {
+    Mesh
+} from "@babylonjs/core/Meshes/mesh";
+import "@babylonjs/core/Meshes/meshBuilder";
+
+import {
     Color3
 } from "@babylonjs/core/Maths/math";
 import {
     StandardMaterial, FresnelParameters
 } from '@babylonjs/core/Materials';
 import {
-    Texture
+    Texture, CubeTexture
 } from '@babylonjs/core/Materials/Textures';
 
 import groundTexture from '../assets/ground.jpg';
@@ -16,7 +21,7 @@ const playerMaterialGen = function (scene) {
     playerMaterial.emissiveColor = new Color3(1, 1, 1);
     playerMaterial.alpha = 0.1;
 
-    const color = Color3.FromInts(100, 100, 59);
+    const color = Color3.FromInts(50, 50, 200);
     playerMaterial.emissiveFresnelParameters = new FresnelParameters();
     playerMaterial.emissiveFresnelParameters.bias = 0.6;
     playerMaterial.emissiveFresnelParameters.power = 2;
@@ -34,9 +39,22 @@ const groundMaterialGen = function (scene) {
     return groundMaterial;
 }
 
+const skyBox = function (scene) {
+    const skybox = Mesh.CreateBox("skyBox", 100.0, scene);
+    const skyboxMaterial = new StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new CubeTexture('src/assets/skybox/SkyBoxTexture', scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new Color3(0, 0, 0);
+    skybox.material = skyboxMaterial;
+    return skybox;
+}
+
 const GameMaterials = function (scene) {
     this.playerMaterial = playerMaterialGen(scene);
     this.groundMaterial = groundMaterialGen(scene);
+    this.skyBox = skyBox(scene);
 }
 
 GameMaterials.prototype.constructor = GameMaterials;
